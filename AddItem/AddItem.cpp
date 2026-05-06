f6c288a05be5607c5d38731778ccbb0b26966004
TGH <2462894979@qq.com>
2026-05-06 16:44:42 +0800

    testt

--- AddItem/AddItem.cpp
+++ AddItem/AddItem.cpp
@@ -2,4 +2,7 @@
 
 void AddItem(Item item)
 {
+    cout << "Add item: " << item.date.year << "-" << item.date.month << "-" << item.date.day
+         << ", reason: " << item.reason
+         << ", amount: " << item.amount << endl;
 }
